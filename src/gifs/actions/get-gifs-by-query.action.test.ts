cc1d2733a8487515e53817d688596957e1ced428
Fernando Herrera <fernando.herrera85@gmail.com>
2025-06-18 09:58:47 -0400

    refactor: improve error handling and return early for empty queries in getGifsByQuery

--- src/gifs/actions/get-gifs-by-query.action.test.ts
+++ src/gifs/actions/get-gifs-by-query.action.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, test } from 'vitest';
+import { beforeEach, describe, expect, test, vi } from 'vitest';
 import AxiosMockAdapter from 'axios-mock-adapter';
 
 import { getGifsByQuery } from './get-gifs-by-query.action';
@@ -7,7 +7,12 @@ import { giphyApi } from '../api/giphy.api';
 import { giphySearchResponseMock } from './../../../tests/mocks/giphy.response.data';
 
 describe('getGifsByQuery', () => {
-  const mock = new AxiosMockAdapter(giphyApi);
+  let mock = new AxiosMockAdapter(giphyApi);
+
+  beforeEach(() => {
+    mock.reset();
+    mock = new AxiosMockAdapter(giphyApi);
+  });
 
   // test('should return a list of gifs', async () => {
   //   const gifs = await getGifsByQuery('goku');
@@ -38,5 +43,33 @@ describe('getGifsByQuery', () => {
       expect(typeof gif.width).toBe('number');
       expect(typeof gif.height).toBe('number');
     });
+  });
+
+  test('should return an empty list of gifs if query is empty', async () => {
+    // mock.onGet('/search').reply(200, { data: [] });
+    mock.restore();
+
+    const gifs = await getGifsByQuery('');
+
+    expect(gifs.length).toBe(0);
+  });
+
+  test('should handle error when the API returns an error', async () => {
+    const consoleErrorSpy = vi
+      .spyOn(console, 'error')
+      .mockImplementation(() => {});
+
+    mock.onGet('/search').reply(400, {
+      data: {
+        message: 'Bad Request',
+      },
+    });
+
+    const gifs = await getGifsByQuery('goku');
+
+    expect(gifs.length).toBe(0);
+    expect(consoleErrorSpy).toHaveBeenCalled();
+    expect(consoleErrorSpy).toHaveBeenCalledTimes(1);
+    expect(consoleErrorSpy).toHaveBeenCalledWith(expect.anything());
   });
 });
